91aa4c91d7127d54ed87ffb4174c33f9dd2495fe
Tiago Almeida <herique.sa@gmail.com>
2021-08-12 16:23:16 -0300

    Service discovery implemented

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+# product-catalog
+
+# shopping-cart
+
+# Service Discovery
+
+Faz a comunicação entre serviços e verifica disponibilidade. Em contra partida adiciona uma camada de rechamadas o que torna a arquitetura mais complexa.
+O spring cloud gerencia essas chamdas e também permite que o SD atue como load balancer.
--- service-dicovery/src/main/java/one/digitalinnovation/experts/servicedicovery/ServiceDicoveryApplication.java
+++ service-dicovery/src/main/java/one/digitalinnovation/experts/servicedicovery/ServiceDicoveryApplication.java
@@ -1,15 +1,16 @@
-package one.digitalinnovation.experts.shoppingcart;
+package one.digitalinnovation.experts.servicedicovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class ShoppingcartApplication {
+@EnableEurekaServer
+public class ServiceDicoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingcartApplication.class, args);
+		SpringApplication.run(ServiceDicoveryApplication.class, args);
 	}
 
 }
